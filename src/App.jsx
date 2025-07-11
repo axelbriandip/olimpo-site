72b7c32be02d86d9af822055571c935abb333ba0
Axel Brian Dip <axelbriandip.rg@gmail.com>
2025-07-11 16:57:53 -0300

    routes add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './styles/App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Router>
+        <nav>Navbar</nav>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/notices" element={<h1>Noticias</h1>} />
+          <Route path="/squads" element={<h1>Equipos</h1>} />
+          <Route path="/about-us" element={<h1>Sobre nosotros</h1>} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+        <footer>Footer</footer>
+      </Router>
     </>
   )
 }
